cd1457deff0cf424a4fe68436b492f0f517c6087
wickapp <wickhomie@gmail.com>
2022-02-11 09:23:27 +0300

    Initial Commit

---
+++ HomeWord 2.7 UITableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWord 2.7 UITableView
+//
+//  Created by Илья Терновской on 11.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
